102c3942646e7da2e6707c77f4d119bd87bb1fa9
Stiven Ramírez Arango <stivenramireza@gmail.com>
2020-04-02 08:36:07 -0500

    Adding a component

--- concepts1/src/app/app.module.ts
+++ concepts1/src/app/app.module.ts
@@ -4,9 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HelloComponent } from './hello/hello.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent
   ],
   imports: [
     BrowserModule,
---
+++ concepts1/src/app/hello/hello.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'custom-hello',
+    template: `<p>Hello world!</p>`
+})
+
+export class HelloComponent {
+
+}
