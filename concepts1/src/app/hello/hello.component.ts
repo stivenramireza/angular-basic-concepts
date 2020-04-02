692e2e923b27f308500891bcb80ea12c5f163a01
Stiven Ramírez Arango <stivenramireza@gmail.com>
2020-04-02 10:15:51 -0500

    ngModel

--- concepts1/src/app/hello/hello.component.ts
+++ concepts1/src/app/hello/hello.component.ts
@@ -14,7 +14,7 @@ export class HelloComponent {
     @Output() sayHello:EventEmitter<string> = new EventEmitter<string>();
 
     public namesList: Array<string> = [];
-    public inputName:string = 'Stiven';
+    public inputName:string = '';
 
     constructor() {
     }
@@ -27,11 +27,6 @@ export class HelloComponent {
         this.namesList.push(this.inputName);
         this.inputName = '';
         console.log(this.namesList)
-    }
-    
-    updateInputName(event:any) {
-        this.inputName = event.target.value;
-        console.log(this.inputName);
     }
 
 }
