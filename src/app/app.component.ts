b50638e09f25c1c15a548c9cd3796620b741de07
Stiven Ramírez Arango <stivenramireza@gmail.com>
2020-04-07 20:31:37 -0500

    ngStyle directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public myName:string = 'Enrique';
+  public titleStyles: {};
+  public counter = 0;
   
   onSayHello(message){
     console.log(message);
+    this.counter++;
+    this.updateTitleStyles();
+  }
+
+  updateNameClasses(name:string) {
+    return {
+      'error': name.length <= 3,
+      'warning': name.length > 3 && name.length <= 6,
+      'success': name.length > 6,
+      'bold': name.length > 8
+    }
+  }
+
+  updateTitleStyles(){
+    this.titleStyles = {
+      'margin-top': '40px',
+      'color': this.counter < 3 ? 'green' : 'goldenrod'
+    }
   }
 }
